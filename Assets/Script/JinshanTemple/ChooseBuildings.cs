f2846c13483781c7f85f82a308c6319885f5541a
hhh688688 <xxxxzzzz688@163.com>
2026-02-07 22:17:06 +0800

    基本操作实现

---
+++ Assets/Script/JinshanTemple/ChooseBuildings.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChooseBuildings : MonoBehaviour
+{
+    [SerializeField] public float boxX, boxY;
+    private bool isDragging=false;
+    private bool hasExecuted = false;
+    [SerializeField] private Vector3 destination;
+    [SerializeField] private Vector3 originalPosition;
+    private Rigidbody rb;
+    private Vector3 currentVelocity;
+    private float smoothForwardTime = 0.3f;
+    private float smoothBackTime = 0.5f;
+    private float maxSpeed = Mathf.Infinity;
+    private float zPosition = 0f;
+    private float distance ;
+    private float r = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        isDragging = false;
+        hasExecuted = false;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, destination);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (isDragging)
+        {
+            mousePosition.z = zPosition;
+            transform.position = mousePosition;
+        }
+
+        if (!isDragging && distance <= r)
+        {
+            transform.position = Vector3.SmoothDamp(
+            transform.position,
+            destination,
+            ref currentVelocity,
+            smoothForwardTime,
+            maxSpeed
+            );
+        }
+        else if(!isDragging && distance >= r&&transform.position!=originalPosition)
+        {
+            transform.position = Vector3.SmoothDamp(
+            transform.position,
+            originalPosition,
+            ref currentVelocity,
+            smoothBackTime,
+            maxSpeed
+            );
+        }
+
+        if (transform.position == originalPosition&& !hasExecuted)
+        {
+            //特效
+            hasExecuted = true;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isDragging=true;
+    }
+
+    private void OnMouseUp()
+    {
+        currentVelocity = rb.velocity;
+        distance =Vector3.Distance( transform.position,destination);
+        isDragging = false;
+        
+    }
+
+    private void OnMouseOver()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (transform.position == originalPosition)
+        {
+            if (mousePosition.x <= originalPosition.x + boxX / 2 && mousePosition.x >= originalPosition.x - boxX / 2
+            && mousePosition.y <= originalPosition.y + boxY / 2 && mousePosition.y >= originalPosition.y - boxY / 2)
+            {
+                Debug.Log("显示资料");
+
+
+            }
+        }
+        
+    }
+}

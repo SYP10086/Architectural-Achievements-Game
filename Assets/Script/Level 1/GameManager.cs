37b33e34e9c22f0380d223d56192030faf058c25
Amazia9 <3380927114@qq.com>
2026-03-09 00:38:50 +0800

    管道逻辑重构
    再次对管道游戏进行重写，现在是可旋转的正方形了（但不能显示水流状态，已经尽力了）

--- Assets/Script/Level 1/GameManager.cs
+++ Assets/Script/Level 1/GameManager.cs
@@ -6,141 +6,149 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    [SerializeField] private LevelData _level;
-    [SerializeField] private Pipe _cellPrefab;
+    [Header("管道预制体")]
+    public GameObject straightPrefab;
+    public GameObject lShapePrefab;
+    public GameObject tShapePrefab;
+    public GameObject crossPrefab;
 
-    private bool hasGameFinished;
-    private Pipe[,] pipes;
-    private List<Pipe> startPipes;
+    [Header("起点与终点预制体")]
+    public GameObject startPrefab; // 新加：起点预制体
+    public GameObject endPrefab;   // 新加：终点预制体
+
+    [Header("关卡设置")]
+    public int width = 4;
+    public int height = 4;
+    public Vector2 startPos = new Vector2(0, 0); // 起点坐标
+    public Vector2 endPos = new Vector2(3, 3);   // 终点坐标
+
+    private Pipe[,] grid;
+    public bool isSolved = false;
+    public bool isStart = false;
 
     private void Awake()
     {
         Instance = this;
-        SpawnLevel();
     }
 
-    private void SpawnLevel()
+    private void Start()
     {
-        pipes = new Pipe[_level.Row, _level.Column];
-        startPipes = new List<Pipe>();
-
-        for (int i = 0; i < _level.Row; i++)
-        {
-            for (int j = 0; j < _level.Column; j++)
-            {
-                Vector2 spawnPos = new Vector2(j + 0.5f, i + 0.5f);
-                Pipe tempPipe = Instantiate(_cellPrefab);
-                tempPipe.transform.position = spawnPos;
-                tempPipe.Init(_level.Data[i * _level.Column + j]);
-                pipes[i, j] = tempPipe;
-                if (tempPipe.PipeType == 1)
-                {
-                    startPipes.Add(tempPipe);
-                }
-            }
-        }
-        Camera.main.orthographicSize = Mathf.Max(_level.Row, _level.Column);
-        Vector3 cameraPos = Camera.main.transform.position;
-        cameraPos.x = _level.Column * 0.5f;
-        cameraPos.y = _level.Row * 0.5f;
-        Camera.main.transform.position = cameraPos;
+        isStart = true;
+        GenerateFixedLevel();
     }
 
     private void Update()
     {
-        if (hasGameFinished) return;
-
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        int row = Mathf.FloorToInt(mousePos.y);
-        int col = Mathf.FloorToInt(mousePos.x);
-        if (row < 0 || col < 0) return;
-        if (row >= _level.Row) return;
-        if (col >= _level.Column) return;
-
-        if (Input.GetMouseButtonDown(0))
+        // 按照需求，每帧检测是否连通
+        if (!isSolved)
         {
-            pipes[row, col].UpdateInput();
-            CheckFill();
-            CheckWin();
-            //StartCoroutine(ShowHint());
+            if (CheckConnection())
+            {
+                isSolved = true;
+                Debug.Log("管道已连通");
+            }
         }
     }
 
-    private IEnumerator ShowHint()
+    // 生成一个固定且有解的4x4关卡
+    private void GenerateFixedLevel()
     {
-        yield return new WaitForSeconds(0.1f);
-        CheckFill();
-        CheckWin();
+        grid = new Pipe[width, height];
+
+        // 定义一个固定的布局 (这里设计了一条从 0,0 到 3,3 的通路)
+        PipeType[,] levelLayout = new PipeType[4, 4]
+        {
+            // Y=0 (底部)
+            { PipeType.Start, PipeType.Straight, PipeType.TShape, PipeType.LShape },
+            // Y=1
+            { PipeType.Straight, PipeType.Cross, PipeType.LShape, PipeType.Straight },
+            // Y=2
+            { PipeType.LShape, PipeType.TShape, PipeType.Straight, PipeType.Cross },
+            // Y=3 (顶部)
+            { PipeType.Cross, PipeType.LShape, PipeType.Straight, PipeType.End }
+        };
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                PipeType type = levelLayout[y, x];
+                GameObject prefabToSpawn = GetPrefabByType(type);
+
+                // 实例化并排列在屏幕上
+                GameObject go = Instantiate(prefabToSpawn, new Vector3(x, y, 0), Quaternion.identity);
+
+                // 【修复点】：尝试获取预制体上自带的 Pipe 组件，如果没有才 AddComponent
+                // 这样可以完美避免挂载两个脚本导致旋转 180° 的 Bug
+                Pipe pipe = go.GetComponent<Pipe>();
+                if (pipe == null)
+                {
+                    pipe = go.AddComponent<Pipe>();
+                }
+
+                pipe.Initialize(type, x, y);
+                grid[x, y] = pipe;
+            }
+        }
+        isStart = false;
     }
 
-    private void CheckFill()
+    private GameObject GetPrefabByType(PipeType type)
     {
-        for (int i = 0; i < _level.Row; i++)
+        switch (type)
         {
-            for (int j = 0; j < _level.Column; j++)
+            case PipeType.Straight: return straightPrefab;
+            case PipeType.LShape: return lShapePrefab;
+            case PipeType.TShape: return tShapePrefab;
+            case PipeType.Cross: return crossPrefab;
+            case PipeType.Start: return startPrefab; // 新加
+            case PipeType.End: return endPrefab;     // 新加
+            default: return straightPrefab;
+        }
+    }
+
+    // 使用DFS（深度优先搜索）检测连通性
+    private bool CheckConnection()
+    {
+        bool[,] visited = new bool[width, height];
+        return DFS((int)startPos.x, (int)startPos.y, visited);
+    }
+
+    private bool DFS(int x, int y, bool[,] visited)
+    {
+        // 如果到达终点，说明连通
+        if (x == (int)endPos.x && y == (int)endPos.y) return true;
+
+        visited[x, y] = true;
+        Pipe currentPipe = grid[x, y];
+
+        // 检查四个方向：0=上, 1=右, 2=下, 3=左
+        int[] dx = { 0, 1, 0, -1 };
+        int[] dy = { 1, 0, -1, 0 };
+        // 对应的邻居需要的反向接口：上对下(2)，右对左(3)，下对上(0)，左对右(1)
+        int[] oppositeDir = { 2, 3, 0, 1 };
+
+        for (int i = 0; i < 4; i++)
+        {
+            // 如果当前管道在这个方向有开口
+            if (currentPipe.connections[i])
             {
-                Pipe tempPipe = pipes[i, j];
-                if (tempPipe.PipeType != 0)
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                // 检查邻居是否在网格内，且未被访问过
+                if (nx >= 0 && nx < width && ny >= 0 && ny < height && !visited[nx, ny])
                 {
-                    tempPipe.IsFilled = false;
+                    Pipe neighborPipe = grid[nx, ny];
+                    // 检查邻居管道在对应的反方向是否有开口
+                    if (neighborPipe.connections[oppositeDir[i]])
+                    {
+                        // 递归继续找
+                        if (DFS(nx, ny, visited)) return true;
+                    }
                 }
             }
         }
-
-        Queue<Pipe> check = new Queue<Pipe>();
-        HashSet<Pipe> finished = new HashSet<Pipe>();
-        foreach (var pipe in startPipes)
-        {
-            check.Enqueue(pipe);
-        }
-
-        while (check.Count > 0)
-        {
-            Pipe pipe = check.Dequeue();
-            finished.Add(pipe);
-            List<Pipe> connected = pipe.ConnectedPipes();
-            foreach (var connectedPipe in connected)
-            {
-                if (!finished.Contains(connectedPipe))
-                {
-                    check.Enqueue(connectedPipe);
-                }
-            }
-        }
-
-        foreach (var filled in finished)
-        {
-            filled.IsFilled = true;
-        }
-
-        for (int i = 0; i < _level.Row; i++)
-        {
-            for (int j = 0; j < _level.Column; j++)
-            {
-                Pipe tempPipe = pipes[i, j];
-                tempPipe.UpdateFilled();
-            }
-        }
-    }
-
-    private void CheckWin()
-    {
-        for (int i = 0; i < _level.Row; i++)
-        {
-            for (int j = 0; j < _level.Column; j++)
-            {
-                if (!pipes[i, j].IsFilled)
-                    return;
-            }
-        }
-
-        hasGameFinished = true;
-        StartCoroutine(GameFinished());
-    }
-
-    private IEnumerator GameFinished()
-    {
-        yield return new WaitForSeconds(2f);
-        UnityEngine.SceneManagement.SceneManager.LoadScene(0);  //后期记得更改胜利后的触发代码
+        return false;
     }
 }
--- Assets/Script/Level 1/Pipe.cs
+++ Assets/Script/Level 1/Pipe.cs
@@ -2,91 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PipeType
+{
+    Straight, // 直管
+    LShape,   // L型管
+    TShape,   // T型管
+    Cross,     // 十字管
+    Start,    // 起点 (新加)
+    End       // 终点 (新加)
+}
+
 public class Pipe : MonoBehaviour
 {
-    [HideInInspector] public bool IsFilled;
-    [HideInInspector] public int PipeType;
+    public PipeType pipeType;
+    public int gridX, gridY;
 
-    [SerializeField] private Transform[] _pipePrefabs;
+    // 数组索引代表方向：0=上, 1=右, 2=下, 3=左
+    public bool[] connections = new bool[4];
 
-    private Transform currentPipe;
-    private int rotation;
-
-    private SpriteRenderer emptySprite;
-    private SpriteRenderer filledSprite;
-    private List<Transform> connectBoxes;
-
-    private const int minRotation = 0;
-    private const int maxRotation = 3;
-    private const int rotationMultiplier = 90;
-
-    public void Init(int pipe)
+    public void Initialize(PipeType type, int x, int y)
     {
-        PipeType = pipe % 10;
-        currentPipe = Instantiate(_pipePrefabs[PipeType], transform);
-        currentPipe.transform.localPosition = Vector3.zero;
-        if (PipeType == 1 || PipeType == 2)
-        {
-            rotation = pipe / 10;
-        }
-        else
-        {
-            rotation = Random.Range(minRotation, maxRotation + 1);
-        }
-        currentPipe.transform.eulerAngles = new Vector3(0, 0, rotation * rotationMultiplier);
+        pipeType = type;
+        gridX = x;
+        gridY = y;
 
-        if (PipeType == 0 || PipeType == 1)
+        // 根据类型初始化开口方向 (默认状态)
+        switch (type)
         {
-            IsFilled = true;
-        }
-
-        if (PipeType == 0)
-        {
-            return;
-        }
-
-        emptySprite = currentPipe.GetChild(0).GetComponent<SpriteRenderer>();
-        emptySprite.gameObject.SetActive(!IsFilled);
-        filledSprite = currentPipe.GetChild(1).GetComponent<SpriteRenderer>();
-        filledSprite.gameObject.SetActive(IsFilled);
-
-        connectBoxes = new List<Transform>();
-        for (int i = 2; i < currentPipe.childCount; i++)
-        {
-            connectBoxes.Add(currentPipe.GetChild(i));
-        }
-    }
-
-    public void UpdateInput()
-    {
-        if (PipeType == 0 || PipeType == 1 || PipeType == 2)
-        {
-            return;
+            case PipeType.Straight:
+                connections = new bool[] { true, false, true, false }; // 上下通
+                break;
+            case PipeType.LShape:
+                connections = new bool[] { true, true, false, false }; // 上右通
+                break;
+            case PipeType.TShape:
+                connections = new bool[] { true, true, true, false };  // 上右下通
+                break;
+            case PipeType.Cross:
+            case PipeType.Start: // 起点和终点在逻辑上设为全通，方便对接任何方向的管道
+            case PipeType.End:
+                connections = new bool[] { true, true, true, true };   // 全通
+                break;
         }
 
-        rotation = (rotation + 1) % (maxRotation + 1);
-        currentPipe.transform.eulerAngles = new Vector3(0, 0, rotation * rotationMultiplier);
-    }
-
-    public void UpdateFilled()
-    {
-        if (PipeType == 0) return;
-        emptySprite.gameObject.SetActive(!IsFilled);
-        filledSprite.gameObject.SetActive(IsFilled);
-    }
-
-    public List<Pipe> ConnectedPipes()
-    {
-        List<Pipe> result = new List<Pipe>();
-        foreach (var box in connectBoxes)
-        {
-            RaycastHit2D[] hit = Physics2D.RaycastAll(box.transform.position, Vector2.zero, 0.1f);
-            for (int i = 0; i < hit.Length; i++)
+        // 随机旋转几次打乱初始状态
+        if (type != PipeType.Start && type != PipeType.End) {
+            int randomRotations = Random.Range(0, 4);
+            if (GameManager.Instance.isStart)
             {
-                result.Add(hit[i].collider.transform.parent.GetComponent<Pipe>());
+                for (int i = 0; i < randomRotations; i++)
+                {
+                    RotateLogic();
+                    transform.Rotate(0, 0, -90f); // 顺时针旋转90度
+                }
             }
         }
+        
+    }
 
-        return result;
+    // 鼠标点击事件（需要物体上有Collider2D）
+    private void OnMouseDown()
+    {
+        if (GameManager.Instance.isSolved || pipeType == PipeType.Start || pipeType == PipeType.End)
+            return;
+
+        // 视觉旋转 (顺时针90度)
+        transform.Rotate(0, 0, -90f);
+        // 逻辑旋转
+        RotateLogic();
+    }
+
+    // 旋转逻辑：顺时针旋转时，开口方向数组向右移位
+    private void RotateLogic()
+    {
+        bool[] newConnections = new bool[4];
+        // 旧的左(3)变成新的上(0)，旧的上(0)变成新的右(1)...
+        for (int i = 0; i < 4; i++)
+        {
+            newConnections[i] = connections[(i + 3) % 4];
+        }
+        connections = newConnections;
     }
 }

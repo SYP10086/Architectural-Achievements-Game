49c7d70dc07b5ff3f8b88640d40a50fd9394a853
Forest-east <2856908425@qq.com>
2026-03-16 22:08:06 +0800

    传一下跳跃动画

--- Assets/Script/Level 1/PlayerController.cs
+++ Assets/Script/Level 1/PlayerController.cs
@@ -1,43 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed;
+    public float speed = 5f;
     private SpriteRenderer SpriteRenderer;
+    public float JumpForce = 3f;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
     Animator animator;
- 
-    // Start is called before the first frame update
+
+    [Header("Ground Detection")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask whatIsGround;
+
     void Start()
     {
-    SpriteRenderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+       
+        CheckGrounded();
+
         float dir = Input.GetAxis("Horizontal");
+
+
+       /* Vector2 velocity = rb.velocity;
+        velocity.x = dir * speed;
+        rb.velocity = velocity;*/
+
+
+        if (isGrounded )
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+      
         if (dir < 0)
         {
             SpriteRenderer.flipX = false;
-            animator.SetBool("IsRunning", true);
-
         }
         else if (dir > 0)
-        { 
-            SpriteRenderer.flipX = true;
-            animator.SetBool("IsRunning", true);
-        }
-        else
         {
-            animator.SetBool("IsRunning", false); 
+            SpriteRenderer.flipX = true;
         }
-            Vector3 movement = new Vector3(speed * dir * Time.deltaTime, 0, 0);
-        transform.Translate(movement);
+
+    
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            animator.SetBool("IsJumping", true);
+            
+        }
+
+    
+        if (rb.velocity.y < -0.1f && !isGrounded)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+            
+        }
     }
-  
+
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+    void CheckGrounded()
+    {
+        
+
+        if (isGrounded)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", false);
+            
+        }
+    }
+    private void FixedUpdate()
+    {
+        float dir = Input.GetAxis("Horizontal");
+
+
+        Vector2 velocity = rb.velocity;
+        velocity.x = dir * speed;
+        rb.velocity = velocity;
+    }
+
 }
+
